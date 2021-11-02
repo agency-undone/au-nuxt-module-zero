aee6485c317315cab3ffa163c29fbb458f43cfa1
Nauras Jabari <nauras@agencyundone.com>
2021-11-01 18:05:31 -0700

    Fix: redirector middleware now handles a lack of or an empty redirects array

--- core/middleware/redirector.js
+++ core/middleware/redirector.js
@@ -1,13 +1,15 @@
 export default function (ctx) {
   const redirects = ctx.$config.redirects
-  let path = ctx.route.fullPath
-  const len = path.length
-  if (path.charAt(len - 1) === '/') {
-    path = path.slice(0, len - 1)
-  }
-  redirects.forEach((redirect) => {
-    if (redirect.from === path) {
-      ctx.redirect(301, redirect.to)
+  if (redirects && redirects.length > 0) {
+    let path = ctx.route.fullPath
+    const len = path.length
+    if (path.charAt(len - 1) === '/') {
+      path = path.slice(0, len - 1)
     }
-  })
+    redirects.forEach((redirect) => {
+      if (redirect.from === path) {
+        ctx.redirect(301, redirect.to)
+      }
+    })
+  }
 }
