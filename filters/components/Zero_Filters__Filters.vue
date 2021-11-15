727623df187228d35c0915cc54e82f1241ccf852
Ben Munoz <benmunozj@gmail.com>
2021-11-15 17:11:15 -0500

    Feat: filter AND/OR combo behaviour setting

--- filters/components/Zero_Filters__Filters.vue
+++ filters/components/Zero_Filters__Filters.vue
@@ -11,12 +11,35 @@
 import { mapGetters, mapActions } from 'vuex'
 
 // =================================================================== Functions
-const projectInclusion = (instance, selection, projectTags) => {
-  if (instance.match === 'and') {
-    return selection.every((val) => { return projectTags.includes(val) })
+const projectIncluded = (selection, tags, type) => {
+  switch (type) {
+    case 'and': return selection.every((val) => { return tags.includes(val) })
+    case 'or': return selection.some((val) => { return tags.includes(val) })
   }
-  return selection.some((val) => { return projectTags.includes(val) })
+  return false
 }
+
+const filterProjects = (selected, collection, matchType) => {
+  const filtered = []
+  if (selected.length) {
+    for (let i = 0; i < collection.length; i++) {
+      const project = collection[i]
+      const projectTags = []
+
+      for (let j = 0; j < project.taxonomies.length; j++) {
+        const tax = project.taxonomies[j]
+        for (let k = 0; k < tax.tags.length; k++) {
+          projectTags.push(tax.tags[k])
+        }
+      }
+      const success = projectIncluded(selected, projectTags, matchType)
+      if (success) { filtered.push(collection[i]) }
+    }
+    return filtered
+  }
+  return collection
+}
+
 
 // ====================================================================== Export
 export default {
@@ -52,32 +75,39 @@ export default {
     settings () {
       return this.siteContent.settings
     },
-    match () {
+    target () {
+      return this.settings.behavior.targetCategoryMatch
+    },
+    targetCategory () {
+      const array = []
+      const category = this.siteContent.taxonomy.categories.find(cat => cat.slug === this.target.categorySlug)
+      category.tags.forEach(tag => array.push(tag.slug))
+      return array
+    },
+    targetSelection () {
+      return this.selected.filter((tag) => this.targetCategory.includes(tag))
+    },
+    primarySelection () {
+      return this.selected.filter((tag) => !this.targetCategory.includes(tag))
+    },
+    primaryMatchType () {
       return this.settings.behavior.tagMatchType
     },
     filtered () {
-      let collection = []
-      if (this.selected.length) {
-        for (let i = 0; i < this.projects.length; i++) {
-          const proj = this.projects[i]
-          const projTags = []
-
-          for (let j = 0; j < proj.taxonomies.length; j++) {
-            const tax = proj.taxonomies[j]
-            for (let k = 0; k < tax.tags.length; k++) {
-              projTags.push(tax.tags[k])
-            }
-          }
-          const success = projectInclusion(this, this.selected, projTags)
-          if (success) { collection.push(this.projects[i]) }
-        }
+      let collection
+      if (this.target.categorySlug) {
+        collection = filterProjects(
+          this.primarySelection,
+          filterProjects(this.targetSelection, this.projects, this.target.tagMatchType),
+          this.primaryMatchType
+        )
       } else {
-        collection = this.projects
+        collection = filterProjects(this.selected, this.projects, this.primaryMatchType)
       }
-
       if (collection.length === 0) { collection = false }
       const payload = { type: 'filtered', collection }
       this.setCollection(payload)
+      console.log(collection)
       return collection
     }
   },
